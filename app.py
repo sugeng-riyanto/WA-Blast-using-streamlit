347ddb3ae907bcf81e0a106504b724c2263525cf
Sugeng Riyanto <36408012+sugeng-riyanto@users.noreply.github.com>
2024-06-27 10:05:15 +0700

    Update app.py

--- app.py
+++ app.py
@@ -4,15 +4,15 @@ import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from openpyxl import load_workbook
+import pywhatkit as kit
 import time
 import warnings
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.chrome.options import Options
+import webbrowser
+import pyautogui
+import os
 
+
+# Suppress specific warnings
 warnings.simplefilter(action='ignore', category=FutureWarning)
 
 # SMTP configuration
@@ -31,13 +31,8 @@ def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 def send_whatsapp_messages(data, announcement=False, invoice=False, proof_payment=False):
-    # Setup Selenium WebDriver with headless Chrome
-    chrome_options = Options()
-    chrome_options.add_argument("--headless")
-    chrome_options.add_argument("--no-sandbox")
-    chrome_options.add_argument("--disable-dev-shm-usage")
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
-    driver.get("https://web.whatsapp.com")
+    # Open WhatsApp Web once
+    webbrowser.open("https://web.whatsapp.com")
     st.info("Please scan the QR code in the opened WhatsApp Web window.")
     
     # Wait for user to scan QR code and login (increase if needed)
@@ -70,9 +65,9 @@ def send_whatsapp_messages(data, announcement=False, invoice=False, proof_paymen
             • *Batas Tanggal Pembayaran:* {row['expired_date']}
             • *Sebesar:* Rp. {row['trx_amount']:,.2f}
             • Pembayaran via nomor *virtual account* (VA) BNI/Bank: *{row['virtual_account']}*
-            Terima kasih atas kerjasamanya.
-            Admin Sekolah
-            Jika ada pertanyaan atau hendak konfirmasi dapat menghubungi:
+        Terima kasih atas kerjasamanya.
+        Admin Sekolah
+        Jika ada pertanyaan atau hendak konfirmasi dapat menghubungi:
             • Ibu Penna (Kasir): https://bit.ly/mspennashb
             • Bapak Supatmin (Admin SMP & SMA): https://bit.ly/wamrsupatminshb4
             """
@@ -95,21 +90,21 @@ def send_whatsapp_messages(data, announcement=False, invoice=False, proof_paymen
         else:
             continue
 
-        try:
-            # Open chat for the phone number
-            search_box = driver.find_element(By.XPATH, '//div[@contenteditable="true"][@data-tab="3"]')
-            search_box.send_keys(phone_number + Keys.ENTER)
-            time.sleep(10)
-            
-            # Send message
-            message_box = driver.find_element(By.XPATH, '//div[@contenteditable="true"][@data-tab="6"]')
-            message_box.send_keys(message + Keys.ENTER)
-            
-            # Wait for a few seconds to ensure the message is sent
-            time.sleep(5)
-            st.success(f"Message sent successfully to {phone_number}")
-        except Exception as e:
-            st.error(f"Failed to send message to {phone_number}: {str(e)}")
+        while True:
+            try:
+                # Send WhatsApp message using the existing session
+                kit.sendwhatmsg_instantly(phone_number, message, wait_time=20)
+                
+                # Wait for 20 seconds to ensure the message is sent
+                time.sleep(20)
+                st.success(f"Message sent successfully to {phone_number}")
+                break  # Exit the loop if the message is sent successfully
+            except Exception as e:
+                st.error(f"Failed to send message to {phone_number}: {str(e)}. Retrying...")
+                time.sleep(20)  # Wait before retrying
+
+
+# Ensure your DataFrame and main application logic
 
 def send_emails(email_list, announcement=False, invoice=False, proof_payment=False):
     for idx, entry in enumerate(email_list):
@@ -162,18 +157,29 @@ def send_emails(email_list, announcement=False, invoice=False, proof_payment=Fal
             <strong>Bapak Supatmin (Admin SMP & SMA):</strong> <a href='https://bit.ly/wamrsupatminshb4' style="color: #007bff;">https://bit.ly/wamrsupatminshb4</a>
             """
         elif proof_payment:
+            subject = entry['Subject']
+            va = entry['virtual_account']
             name = entry['Nama_Siswa']
             email = entry['Email']
+            grade = entry['Grade']
+            sppbuljal = "{:,.2f}".format(entry['bulan_berjalan'])
+            ket1 = entry['Ket_1']
+            spplebih = "{:,.2f}".format(entry['SPP_30hari'])
+            ket2 = entry['Ket_2']
+            denda = "{:,.2f}".format(entry['Denda'])
+            ket3 = entry['Ket_3']
+            ket4 = entry['Ket_4']
+            total = "{:,.2f}".format(entry['Total'])
             message = f"""
-            Kepada Yth.<br>Orang Tua/Wali Murid <span style="color: #007bff;">{name}</span><br>
+            Kepada Yth.<br>Orang Tua/Wali Murid <span style="color: #007bff;">{name}</span> (Kelas <span style="color: #007bff;">{grade}</span>)<br>
             <p>Salam Hormat,</p>
-            <p>Kami hendak menyampaikan info mengenai:</p>
+            <p>Kami hendak menyampaikan info mengenai SPP:</p>
             <ul>
-                <li>SPP yang sedang berjalan: {entry['bulan_berjalan']}, {entry['Ket_1']}</li>
-                <li>Denda: {entry['Denda']}, {entry['Ket_3']}</li>
-                <li>SPP bulan-bulan sebelumnya: {entry['SPP_30hari']}, {entry['Ket_2']}</li>
-                <li>Keterangan: {entry['Ket_4']}</li>
-                <li>Total tagihan: {entry['Total']}</li>
+                <li><strong>SPP yang sedang berjalan:</strong> {sppbuljal} ({ket1})</li>
+                <li><strong>Denda:</strong> {denda} ({ket3})</li>
+                <li><strong>SPP bulan-bulan sebelumnya:</strong> {spplebih} ({ket2})</li>
+                <li><strong>Keterangan:</strong> {ket4}</li>
+                <li><strong>Total tagihan:</strong> {total}</li>
             </ul>
             <p>Terima kasih atas kerjasamanya.</p>
             <p>Admin Sekolah</p>
@@ -192,30 +198,47 @@ def send_emails(email_list, announcement=False, invoice=False, proof_payment=Fal
 
         try:
             server.sendmail(your_email, email, msg.as_string())
-            st.success(f'Email sent to {email}')
+            st.success(f'Email {idx + 1} to {email} successfully sent!')
         except Exception as e:
-            st.error(f'Failed to send email to {email}: {str(e)}')
+            st.error(f'Failed to send email {idx + 1} to {email}: {e}')
 
-st.title('Sekolah Harapan Bangsa Messaging System')
+def handle_file_upload(announcement=False, invoice=False, proof_payment=False):
+    uploaded_file = st.file_uploader("Upload Excel file", type="xlsx")
+    if uploaded_file is not None:
+        df = pd.read_excel(uploaded_file)
+        email_list = df.to_dict(orient='records')
+        st.dataframe(df)
 
-uploaded_file = st.file_uploader('Upload your Excel file', type=['xlsx'])
-
-if uploaded_file is not None:
-    file_name = uploaded_file.name
-    if allowed_file(file_name):
-        data = pd.read_excel(uploaded_file)
-
-        st.subheader('Choose your action')
-        announcement = st.checkbox('Send Announcement')
-        invoice = st.checkbox('Send Invoice')
-        proof_payment = st.checkbox('Send Proof of Payment')
-
-        if st.button('Send WhatsApp Messages'):
-            send_whatsapp_messages(data, announcement, invoice, proof_payment)
-        if st.button('Send Emails'):
-            email_list = data.to_dict('records')
+        if st.button("Send Emails"):
             send_emails(email_list, announcement, invoice, proof_payment)
-    else:
-        st.error('Invalid file type. Please upload an Excel file.')
+        
+        if st.button("Send WhatsApp Messages"):
+            send_whatsapp_messages(df, announcement, invoice, proof_payment)
 
-server.quit()
+def main():
+    st.title('Communication Sender for SHB')
+
+    menu = ["Home", "Invoice", "Send Reminder", "Announcement"]
+    choice = st.sidebar.selectbox("Menu", menu)
+
+    if choice == "Home":
+        st.subheader("Home")
+        st.write("Welcome to the Communication Sender App!")
+
+    elif choice == "Announcement":
+        st.subheader("Announcement")
+        handle_file_upload(announcement=True)
+
+    elif choice == "Invoice":
+        st.subheader("Invoice")
+        handle_file_upload(invoice=True)
+
+    elif choice == "Send Reminder":
+        st.subheader("Send Reminder")
+        handle_file_upload(proof_payment=True)
+
+    # Add link to download template Excel file
+    st.markdown("[Download Template Excel file](https://drive.google.com/drive/folders/1Pnpmacr7n3rS1Uht8eUI8A75KFrSA7rt?usp=sharing)")
+
+if __name__ == '__main__':
+    main()
